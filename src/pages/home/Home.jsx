3fdab03ff6b549a8c6769ddd600cf51a6863bd56
isamidinovas <99507186+isamidinovasymbatt@users.noreply.github.com>
2023-02-17 20:22:57 +0600

    added section About

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import About from "../../components/HomePage/About/About";
+
+const Home = () => {
+  return (
+    <>
+      <About/>
+    </>
+  );
+};
+
+export default Home;
