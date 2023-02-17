19a379196d45c38a2b5046f810f06afb6c26de2f
isamidinovas <99507186+isamidinovasymbatt@users.noreply.github.com>
2023-02-18 00:35:38 +0600

    added Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./Button.module.scss";
+const Button = ({ text }) => {
+  return <button className={styles.btn}>{text}</button>;
+};
+
+export default Button;
