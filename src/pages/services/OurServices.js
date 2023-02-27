dbe6b8db97c6123c638e33f37650be7146bfe471
Aman710-hub <amantur710@gmail.com>
2023-02-27 16:35:36 +0600

    feat: add backgrond images for header

--- src/pages/services/OurServices.js
+++ src/pages/services/OurServices.js
@@ -4,17 +4,22 @@ import styles from "./OurServices.module.scss";
 const OurServices = () => {
   return (
     <>
-      <section className={styles.container}>
-        <header className={styles.header}>
-          <h2>Наши услуги</h2>
-          <p>
-            Наши сотрудники прошли сертифицированные тренинги в Учебных центрах
-            ГК DoorHan в г. Москва, г. Алматы, г. Астаны а так же успешно сдали
-            экзамены и являются обладателями сертификатов по направлениям
-            «Воротные системы, ролл ставни, ролл ворота, автоматические
-            системы», «Монтаж автоматики».
-          </p>
-        </header>
+      <section className={styles.background_img_container}>
+        <div className={styles.container}>
+          <header className={styles.header}>
+            <h2>Наши услуги</h2>
+            <p>
+              Наши сотрудники прошли сертифицированные тренинги в Учебных
+              центрах ГК DoorHan в г. Москва, г. Алматы, г. Астаны
+              а так же успешно сдали экзамены и являются обладателями
+              сертификатов по направлениям «Воротные системы, ролл ставни, ролл
+              ворота, автоматические системы», «Монтаж автоматики».
+            </p>
+          </header>
+          <div className={styles.servicesList}>
+            
+          </div>
+        </div>
       </section>
     </>
   );
