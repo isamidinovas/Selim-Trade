15a90b0f58693804d083733ed1ff920504a975fb
isamidinovas <99507186+isamidinovasymbatt@users.noreply.github.com>
2023-03-15 21:08:22 +0600

    fix:merging branches err

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -168,24 +168,6 @@ const Footer = () => {
             <div className={styles.footer_col_4}>{/* <Map /> */}</div>
           </div>
 
-          <div className={styles.footer_col_4}>
-            <iframe
-              style={{ borderRadius: "10px" }}
-              width="100%"
-              height="100%"
-              frameborder="0"
-              scrolling="no"
-              marginheight="0"
-              marginwidth="0"
-              id="gmap_canvas"
-              src="https://maps.google.com/maps?width=738&amp;height=377&amp;hl=en&amp;q=%D0%90%D0%B9%D0%BD%D0%B8%2022%20Bishkek+(Selim%20Trade)&amp;t=p&amp;z=18&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-            ></iframe>
-            <div className={styles.footer_col_4_text}>
-              <p>АДРЕС</p>
-              <p>г. Бишкек, ул. Айни 22</p>
-            </div>
-          </div>
-        </div>
           <span className={styles.location_pin}>
             <svg
               width="329"
