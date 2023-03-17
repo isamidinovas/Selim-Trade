e401abbb2f07ebad7d6c42d003ecb33f5892108e
isamidinovas <99507186+isamidinovasymbatt@users.noreply.github.com>
2023-03-17 17:23:38 +0600

    feat: add redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+// import { store } from "./store/store";
+
 import App from "./App";
 import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    {/* <Provider store={store}> */}
     <App />
+    {/* </Provider> */}
   </BrowserRouter>
 );
