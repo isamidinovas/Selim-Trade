42d1a3d739bd6fd6c96a8301fe0430932dcd5f58
Amantur <amantur710@gmail.com>
2023-03-20 10:35:55 +0600

    feat: set up admin slice

--- src/admin/routes/ProtectedRoute.jsx
+++ src/admin/routes/ProtectedRoute.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import Authentication from "../pages/authentication/Authentication";
 
 const ProtectedRoute = () => {
-  const [isA, setIsA] = useState(false);
-  return <div>{isA ? <Authentication /> : "not aut"}</div>;
+  const [isA, setIsA] = useState(true);
+  return <div>{isA ? "you are authorized" : <Authentication />}</div>;
 };
 
 export default ProtectedRoute;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-// import { store } from "./store/store";
+import { store } from "./redux/store";
 
 import App from "./App";
 import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
