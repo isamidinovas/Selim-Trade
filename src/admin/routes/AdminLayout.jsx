8d7031815ec1032b43095ec808b199298d6d624e
Amantur <amantur710@gmail.com>
2023-04-02 10:35:25 +0600

    feat(admin) abjust sidebar layout

--- src/admin/routes/AdminLayout.jsx
+++ src/admin/routes/AdminLayout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router";
 import SideBar from "../components/SideBar/SideBar";
-const MemoizedSideBar = React.memo(SideBar);
 const AdminLayout = () => {
   return (
     <>
