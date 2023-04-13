fe614b7f1028147056814971c8bf2144c9ca925e
Amantur <amantur710@gmail.com>
2023-04-13 13:25:29 +0600

    feat: add orders page

--- src/redux/admin/adminOrdersSlice.js
+++ src/redux/admin/adminOrdersSlice.js
@@ -3,6 +3,8 @@ import customFetch from "../../utils/axios";
 
 const initialState = {
   orders: [],
+  orderHistory: null,
+  currentStatus: null,
 };
 
 export const getOrders = createAsyncThunk(
@@ -20,12 +22,54 @@ export const getOrders = createAsyncThunk(
     }
   }
 );
+export const getOrderHistory = createAsyncThunk(
+  "adminOrders/getOrderHistory",
+  async (id, thunkAPI) => {
+    try {
+      const response = await customFetch.get(
+        `/api/v1/protected/orders/${id}/history`,
+        {
+          headers: {
+            authorization: `Bearer ${thunkAPI.getState().admin.token}`,
+          },
+        }
+      );
+      console.log(response.data.content);
+      return response.data;
+    } catch (error) {
+      return error.response;
+    }
+  }
+);
+export const getCurrentStatus = createAsyncThunk(
+  "adminOrders/getCurrentStatus",
+  async (id, thunkAPI) => {
+    try {
+      const response = await customFetch.get(`/api/v1/protected/orders/${id}`, {
+        headers: {
+          authorization: `Bearer ${thunkAPI.getState().admin.token}`,
+        },
+      });
+      console.log(response.data.content);
+      return response.data;
+    } catch (error) {
+      return error.response;
+    }
+  }
+);
 
 const adminOrdersSlice = createSlice({
   name: "adminOrders",
   initialState,
   reducers: {},
   extraReducers: {
+    [getCurrentStatus.fulfilled]: (state, { payload }) => {
+      console.log(payload);
+      state.currentStatus = payload;
+    },
+    [getOrderHistory.fulfilled]: (state, { payload }) => {
+      state.orderHistory = payload.content;
+    },
     [getOrders.fulfilled]: (state, { payload }) => {
       state.orders = payload.content;
     },
